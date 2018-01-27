cf53006c8291e53f080daf22f03d27e94090bb70
Allie Williams <AllieMac@Allies-MacBook-Pro.local>
2018-01-26 17:01:50 -0700

    racer game

--- js/racerscripts.js
+++ js/racerscripts.js
@@ -1,6 +1,6 @@
   //run this whole thing when doc loads
 $(document).ready(function(){
-  
+    alert("tap your key alot to win! 3.... 2.... 1.... GO!!!");
     //grab both div htmls and chnage css 
     $("div").css("position", "relative");
 
@@ -9,13 +9,12 @@ $(document).ready(function(){
     function move(e){
         //if n call moveThis func w/ n as argument
         //function is cathcing the thing the bind is giving us
-        if(e.which==78) {
-            moveThis("n");
-
+        if(e.which==65) {
+            moveThis("a");
         }
         //if s call moveThis func w/ s as argument
-        else if ( e.which==83) {
-            moveThis("s");
+        else if ( e.which==76) {
+            moveThis("l");
         }
     }
     function moveThis(key){
@@ -25,13 +24,13 @@ $(document).ready(function(){
         //declare var divSelectionText and give it intial val of empty string
         var divSelectionText = "";
         //key argument === n , using letter to know which player and grabbing their div
-        if(key === "n"){
+        if(key === "a"){
         //storing player 1's id AS A STRING!!! in divSelectionText variable
-            divSelectionText = '#divOne';
+            divSelectionText = '#racerOne';
         }
         //if key argument === s store player 2's id in divSelectionText variable
-        else if(key === "s"){
-            divSelectionText = '#divTwo';
+        else if(key === "l"){
+            divSelectionText = '#racerTwo';
         }
         /*
         we wanted to add to the current value of the left property
@@ -41,7 +40,7 @@ $(document).ready(function(){
 
         // store diveToMove's parsed left css value (now a number) in currentLeft Variable
         var currentLeftLocation = parseInt($(divSelectionText).css("left")); //"left" gives the value of {left: 20px} 20px needs to be parsed into a number
-        //jQueyr is using divSelectionText variable and add +20 to currentleft (a variable) and concatenate it with px to make it work in css ("has to be a string")
+        //jQuery is using divSelectionText variable and add +20 to currentleft (a variable) and concatenate it with px to make it work in css ("has to be a string")
         //contains a srting with the name of the div whose css we want to alter, when the string is passed into jQuery becomes the html element and
         //                          ....here we unParse currentLeft and turn it back into a string that can be used in css (something px)
         $(divSelectionText).css("left", (currentLeftLocation+2) + "px");
@@ -57,6 +56,9 @@ $(document).ready(function(){
             //divSelection text contains a string, the string is the div's id, we use it in the jQuery line...
             //...and it becomes an html element, and then we grab the id of that element ... ;) then we concatenate it into another string to display the winner!!
             $("span").text($(divSelectionText).attr('id') + "reached 300px!");
+            $("span").css("color","red");
+            $("span").css("font-size","150px");
+            $
             //want to end the function
             $(document).unbind();
             //unbind removes the ability of a keystroke to call the code
